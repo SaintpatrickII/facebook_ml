d916f1dbad9bfff1eeb17bc3ee77ae2eba181466
Patrick Govus <patrickgovus99@gmail.com>
2022-05-16 21:33:41 +0100

    final image script, needs docstrings

--- multiclass_logreg.py
+++ multiclass_logreg.py
@@ -9,6 +9,7 @@ import torch
 from torchvision.transforms import ToTensor
 from torch import flatten
 import os
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
@@ -18,79 +19,76 @@ images_corrected = '/Users/paddy/Desktop/AiCore/facebook_ml/Images'
 
 df = pd.read_csv(data)
 
-class image_setup:
-    def __init__(self, df: pd):
-        self.df = df
-        self.image_list_fixed = glob.glob(f'{images_corrected}/*.jpg')
-        self.image_id_list = []
-        self.class_list = []
-        self.flattened = []
-        self.list_of_tuples = []
-        self.df_cleaned(df)
-        self.classification_names_list(df)
-        self.image_correct_prop()
-        self.image_to_tensor()
-        self.training_tuples()
-
+class image_setup():
     
-    def df_cleaned(self, df: pd):
-        self.df = self.df.drop(['Unnamed: 0', 'bucket_link', 'image_ref', 'create_time'], axis=1)
-        self.df = self.df.drop(['uuid', 'price', 'id.1'], axis=1)
-        column_indicies = [1,2,3,4,5,6,7,8,9,10,11,12]
-        classification_columns = ['0','1','2','3','4','5','6','7','8','9','10','11']
-        old_names = self.df.columns[column_indicies]
-        self.df.rename(columns=dict(zip(old_names, classification_columns)),inplace=True)
-        self.df = self.df.drop(['id'], axis=1)
+    def __init__(self):
+        image_list = glob.glob(f'{images}/*.jpg')
+        df = pd.read_json(data)
+        self.image_id =  df['image_id'].to_numpy()
+        self.image_array = []
+        self.image_classification = df['category'].to_numpy()    
+        self.list_of_tuples = []
 
-
-    def classification_names_list(self, df: pd):
-        self.df['new'] = self.df.apply(lambda x: x.index[x == 1].tolist(), axis=1)
-        flat_list = []
-        class_list = self.df['new'].tolist()
-        for sublist in class_list:
-            for item in sublist:
-                flat_list.append(item)
-            self.class_list = list(map(int, flat_list))
+        np.vectorize(self.image_correct_prop())
+        self.image_to_tensor()
+        self.joiner()
+        self.logreg()
 
 
     def image_correct_prop(self):
         resized_img = 128
-        for n, img in enumerate(image_list[:50], 1):
-            image_ite = Image.open(img)
+        for img in range(len(self.image_id)):
+            image_ite = Image.open(f'{images}/{self.image_id[img]}.jpg')
             background = Image.new(mode='RGB', size=(resized_img, resized_img)) 
             original_img = image_ite.size
             max_dim = max(image_ite.size)
             ratio = resized_img / max_dim
             img_corr_ratio =(int(original_img[0] * ratio), int(original_img[1] * ratio))
             img_corr = image_ite.resize(img_corr_ratio)
-            # print(img_corr)
             background.paste(img_corr, (((resized_img - img_corr_ratio[0]) // 2), ((resized_img - img_corr_ratio[1]) // 2)))
-            # print(background)
-            background.save(f'{images_corrected}/{img[50:]}')
+            background.save(f'{images_corrected}/{self.image_id[img]}.jpg')
 
-    
+
     def image_to_tensor(self):
-        for i, img in enumerate(self.image_list_fixed[:50]):
-            image = Image.open(img)
+        for img in self.image_id:
+            image = Image.open(f'{images_corrected}/{img}.jpg')
+            print(image)
             t = ToTensor()
             tensor = t(image)
             flattened = torch.flatten(tensor)
-            # print(self.flattened)
-            self.flattened.append(flattened)
-
-        return self.flattened
-
-    def training_tuples(self):
-        for i in range(6266):
-                # print(image_array[i])
-                # print(image_array[i])
-                ready_tuple = (self.flattened[i], self.class_list[i])
-                self.list_of_tuples.append(ready_tuple)
-        return self.list_of_tuples
-        
+            flatten_numpy = flattened.numpy()
+            self.image_array.append(flatten_numpy)
+            print(flatten_numpy)
+        return self.image_array
 
 
-            
-    
-    pass
-image_setup(df)
+
+    def joiner(self):
+        for i in range(len(self.image_array)):
+            classification = self.image_classification[i]
+            image_tuple = self.image_array[i], classification
+            self.list_of_tuples.append(image_tuple)
+        return 
+
+
+    def logreg(self):
+        m = len(self.list_of_tuples)
+        array_size = 49152
+        self.list_of_tuples
+        print(len(self.list_of_tuples))
+        X = np.zeros((m, array_size))
+        y = np.zeros(m)
+        print(X.shape, y.shape)
+        for item in range(m):
+            features, labels = self.list_of_tuples[item]
+            X[item, :] = features
+            y[item] = labels
+        model = LogisticRegression(max_iter= 100)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=37)
+        model.fit(X_train, y_train)
+        pred = model.predict(X_test)
+        print ('accuracy:', accuracy_score(y_test, pred))
+        report = ('report:', classification_report(y_test, pred))
+        return report
+
+   
