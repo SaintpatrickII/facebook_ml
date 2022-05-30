6009a86afa7636fe1451b61e788b2c86348223e0
Patrick Govus <patrickgovus99@gmail.com>
2022-05-30 19:35:31 +0100

    fixed mostly

--- CNN_transfer_learning.py
+++ CNN_transfer_learning.py
@@ -29,59 +29,115 @@ val_samples = DataLoader(val_data, batch_size=batch_size)
 test_samples = DataLoader(test_data, batch_size=batch_size)
 
 #%%
+
+
+
+
+model = models.resnet50(pretrained=True)
+print(model)
+# update model parametersby adding conv layer at the end
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
 # print(dir(models.resnet50()))
-class resnet50CNN(torch.nn.Module):
-    def __init__(self, out_size):
-        super().__init__()
-        self.features = models.resnet50(pretrained=True).named_modules
-        self.features.fc = torch.nn.Linear(2048, 13)
-         # get the convolutional layers of resnet50. output size is 7x7x2048
-        self.regressor = torch.nn.Sequential(
-            torch.nn.Linear(7*7*2048, 4096), # first arg is the size of the flattened output from resnet50
-            torch.nn.ReLU(),
-            torch.nn.Dropout(),
-            torch.nn.Linear(4096, 1024),
-            torch.nn.ReLU(),
-            torch.nn.Linear(1024, out_size),
-            torch.nn.Softmax(dim=1)
-        )
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.features = models.resnet50(pretrained=True).to(device)
+        for i, param in enumerate(self.features.parameters()):
+            if i < 47:
+                param.requires_grad=False
+            else:
+                param.requires_grad=True
+        self.fc = torch.nn.Linear(125, 13)
+        self.neural_layer = nn.Sequential(nn.Linear(1000, 500), # first arg is the size of the flattened output from resnet50
+        torch.nn.ReLU(),
+        torch.nn.Dropout(),
+        torch.nn.Linear(500, 125),
+        torch.nn.ReLU())
+        
+           
 
     def forward(self, x):
-        x = F.relu(self.features(x))
-        x = x.reshape(-1, 7*7*2048)
-        x = self.regressor(x)
+        x = self.features(x)
+        x = self.neural_layer(x)
+        x = self.fc(x)
+        x = x.reshape(x.shape[0], -1)
+        # x = torch.nn.Linear(256, 13),
+        # x = torch.nn.Softmax(dim=1)
+        # predict = self.fc(x)
+        # fully_connected = x
+        # print(x)
         return x
 
-    def freeze(self):
-        for param in self.features.parameters():
-            param.requires_grad=False
-
-    def unfreeze(self):
-        for param in self.features.parameters():
-            param.requires_grad=True
+model = CNN()
 
 
-cnn = resnet50CNN(13)
-print(dir(cnn.features))
+cnn = CNN()
+cnn.get_features = True
+print(cnn.parameters())
 
-print(cnn.features)
+# print(cnn.features)
 #%%
 
 def train_model(model, epochs):
 
-    optimiser = optim.SGD(model.parameters(), lr=0.1)
+    optimiser = optim.SGD(model.parameters(), lr=0.01)
     for epoch in range(epochs):
-        for batch in train_samples:
+        for i, batch in enumerate(train_samples):
+        # [train_samples, val_samples]:
+        #     if batch == train_samples:
+        #         model.train()
+        #     else:
+        #         model.eval()
+
             features, labels = batch
             predict = model(features)
             loss = F.cross_entropy(predict, labels)
-            loss.backward()
-            optimiser.step()
-            optimiser.zero_grad()
+            if epoch == train_samples:
+                loss.backward()
+                optimiser.step()
+                optimiser.zero_grad()
 
 
-            if batch % 500 == 499:    # print every 50 mini-batches
-               print(f'[{epoch + 1}, {batch + 1:5d}] loss: {loss}')
+            if i % 100 == 99:   
+                print(batch) # print every 50 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {loss}')
             
 
-train_model(cnn, 5)
+train_model(cnn, 1)
+
+# %%
+
+#%%
+
+
+def check_accuracy(loader, model):
+    if loader == train_samples:
+        model.train()
+        print('Checking accuracy on training set')
+    else:
+        print('Checking accuracy on test set')
+        model.eval()
+    num_correct = 0
+    num_samples = 0
+      # set model to evaluation mode
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device)  # move to device
+            y = y.to(device)
+            scores = model(x)
+            print(scores.max(1))
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print(f'Got {num_correct} / {num_samples} with accuracy: {acc}')
+
+
+
+check_accuracy(train_samples, model)
+check_accuracy(test_samples, model)
+#%%
