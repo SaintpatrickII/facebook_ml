8cb8a23a78320e9f203b76be59a8ea9bf5788901
Patrick Govus <92804317+SaintpatrickII@users.noreply.github.com>
2022-06-21 19:57:16 +0100

    need fix on training loop & docstrings

--- NLP_CNN.py
+++ NLP_CNN.py
@@ -8,7 +8,9 @@ import torchtext
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torch.utils.data import DataLoader, Dataset
-
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+import torch.nn.functional as F
 
 
 products = '/Users/paddy/Desktop/AiCore/facebook_ml/nlp_fin.pkl'
@@ -91,20 +93,97 @@ class CNN(torch.nn.Module):
 
 
     def forward(self, X):
-        print(X.shape)
+        # print(X.shape)
         X = self.embedding(X)
         X = X.transpose(2, 1)
-        print(X.shape)
-        print(X)
+        # print(X.shape)
+        # print(X)
         return self.layers(X)
 
 cnn = CNN()
 
 
-example = dataset[1]
-description, category = example
-prediction = cnn(description.unsqueeze(0))
-print(prediction)
-print(category)
+# example = dataset[1]
+# description, category = example
+# prediction = cnn(description.unsqueeze(0))
+# print(prediction)
+# print(category)
+
+
+
+train_split = 0.7
+validation_split = 0.15
+batch_size = 64
+
+data_size = len(dataset)
+print(f'dataset contains {data_size} Images')
+
+train_size = int(train_split * data_size)
+val_size = int(validation_split * data_size)
+test_size = data_size - (val_size + train_size)
+train_data, val_data, test_data = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
+
+train_samples = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+val_samples = DataLoader(val_data, batch_size=batch_size)
+test_samples = DataLoader(test_data, batch_size=batch_size)
+
+
+
+
+#%%
+def train_model(model, epochs):
+    writer = SummaryWriter()
+    model.train()
+    print('training model')
+    optimiser = optim.SGD(model.parameters(), lr=0.01)
+    for epoch in range(epochs):
+        print(f'Epoch {epoch + 1}/{epochs}')
+        print('-' * 10)
+        for phase in [train_samples, val_samples]:
+            if phase == train_samples:
+                print('training')
+            else:
+                print('val')
+            for i, (features, labels) in enumerate(phase):
+                num_correct = 0
+                num_samples = 0
+                print(features)
+                print(labels)
+                # features, labels = features, labels
+                # features = features.to(device)  # move to device
+                # labels = labels.to(device)
+                predict = model(features.unsqueeze(0))
+                # predict = predict.type(torch.LongTensor)
+                print('this is label:', labels)
+                print(labels.shape)
+                print('this is predict:', predict)
+                print(predict.shape)
+                labels = labels
+                loss = F.cross_entropy(predict, labels)
+                _, preds = predict.max(1)
+                num_correct += (preds == labels).sum()
+                num_samples += preds.size(0)
+                acc = float(num_correct) / num_samples
+                loss.backward()
+                optimiser.step()
+                optimiser.zero_grad()
+                # writer.add_scalar('Loss', loss, epoch)
+                # writer.add_scalar('Accuracy', acc, epoch)
+                if i % 10 == 9:
+                    if phase == train_samples:
+                      writer.add_scalar('Training Loss', loss, epoch)
+                      writer.add_scalar(' Training Accuracy', acc, epoch)
+                      print('training_loss')
+                    else:
+                      writer.add_scalar('Validation Loss', loss, epoch)
+                      writer.add_scalar('Validation Accuracy', acc, epoch)
+                      print('val_loss') 
+                    # print(batch) # print every 50 mini-batches
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {loss}')
+                    print(f'Got {num_correct} / {num_samples} with accuracy: {acc * 100}%')
+                    writer.flush()
+            
+
+train_model(cnn, 50)
 
 #%%
